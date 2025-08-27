727699bcf2923839b4fe55ecad74bb490a39b441
SUSANMWANGI <susanwangarimwngi@gmail.com>
2025-08-27 13:02:00 +0300

    Correctly imported FILES  into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import About from "./components/About.jsx"
 import Header from "./components/Header.jsx"
-import Projects from "./components/projects.jsx"
+import Projects from "./components/Projects.jsx"
 import Testimonials from "./components/Testimonials.jsx"
-import Contact from "./components/contact.jsx"
+import Contact from "./components/Contact.jsx"
 import Footer from "./components/Footer.jsx"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
