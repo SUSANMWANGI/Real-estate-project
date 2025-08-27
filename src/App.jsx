6959a1c72edb45e78b88e04d48c52428da065eba
SUSANMWANGI <susanwangarimwngi@gmail.com>
2025-08-27 13:04:27 +0300

    Correctly imported FILES  into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About.jsx"
 import Header from "./components/Header.jsx"
-import Projects from "./components/Projects.jsx"
+import Projects from "./components/projects.jsx"
 import Testimonials from "./components/Testimonials.jsx"
 import Contact from "./components/Contact.jsx"
 import Footer from "./components/Footer.jsx"
